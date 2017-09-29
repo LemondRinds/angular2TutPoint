8b4c56797c16d4f42841eaa34ee9aaad8deecda7
LemondRinds <diveaway89@gmail.com>
2017-09-29 16:00:56 -0400

    some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,31 @@
 import { Component } from '@angular/core';
-
+// metadata for the component
 @Component({
+	// 'css like' selector, angular creates
+	// this component on the first matched dom element
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  // template url
+  templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+// das class
+export class AppComponent  {
+	// metadata via constructor
+	//constructor(@Environment(‘test’ private appTitle:string) { }
+	// basic typescript property
+	name: string = 'Angular';
+	// metadata as decorator
+	//@Environment(‘test’)
+	appTitle: string = 'Title';
+	// prop used for *ngIf
+	appStatus: boolean = true;
+	// prop used for *ngFor
+	appList: any[] = [ {
+      "ID": "1",
+      "Name" : "One"
+   },
+
+   {
+      "ID": "2",
+      "Name" : "Two"
+   } ];
+}
